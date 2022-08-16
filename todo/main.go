ece770d5734bfa73a42f8c1fa9f3ddf2d6f4f988
Simeon Widdis <sawiddis@gmail.com>
2022-08-15 18:30:24 -0600

    Add Healthcheck endpoint

--- todo/main.go
+++ todo/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/Swiddis/octodo/todo/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, World!",
-		})
-	})
+	r.GET("/healthcheck", handlers.Healthcheck)
 	r.Run()
 }
