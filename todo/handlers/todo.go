60e6ce6986ffa9c9c72a33f7df1a45855b1abdaf
Simeon Widdis <sawiddis@gmail.com>
2022-08-15 19:23:54 -0600

    Add Todo PUT endpoint

--- todo/handlers/todo.go
+++ todo/handlers/todo.go
@@ -36,6 +36,21 @@ func PostTodo(c *gin.Context) {
 	c.JSON(201, todo)
 }
 
+func PutTodo(c *gin.Context) {
+	todo := model.Todo{}
+	err := c.ShouldBindJSON(&todo)
+	if Check(err, "unable to parse Todo", c) {
+		return
+	}
+	rawId := c.Param("id")
+	id, err := strconv.ParseUint(rawId, 10, 0)
+	if Check(err, "unable to parse id", c) {
+		return
+	}
+	todo.Id = id
+	c.JSON(200, todo)
+}
+
 func DeleteTodo(c *gin.Context) {
 	rawId := c.Param("id")
 	_, err := strconv.ParseUint(rawId, 10, 0)
--- todo/main.go
+++ todo/main.go
@@ -10,6 +10,7 @@ func main() {
 	r.GET("/healthcheck", handlers.Healthcheck)
 	r.GET("/todo/:id", handlers.GetTodo)
 	r.POST("/todo", handlers.PostTodo)
+	r.PUT("/todo/:id", handlers.PutTodo)
 	r.DELETE("/todo/:id", handlers.DeleteTodo)
 	r.Run()
 }
