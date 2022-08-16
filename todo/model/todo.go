bdea669b6780980c0144743a38ab9cc9b5ebcef8
Simeon Widdis <sawiddis@gmail.com>
2022-08-15 18:29:31 -0600

    Add GetDemoTodo method

--- todo/model/todo.go
+++ todo/model/todo.go
@@ -6,3 +6,12 @@ type Todo struct {
 	Description string `json:"description"`
 	IsDone      bool   `json:"done"`
 }
+
+func GetDemoTodo() Todo {
+	return Todo{
+		Id:          1,
+		Title:       "Implement REST for Octodo",
+		Description: "We have a solid idea, we just need to implement it!",
+		IsDone:      false,
+	}
+}
